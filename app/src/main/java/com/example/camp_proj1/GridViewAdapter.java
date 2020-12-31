9ceb5dc191b5246d93fadaa9d46fbc2fe2966850
jsj7878 <61611131+jsj7878@users.noreply.github.com>
2020-12-31 16:11:07 +0900

    complete tab2

--- app/src/main/java/com/example/camp_proj1/GridViewAdapter.java
+++ app/src/main/java/com/example/camp_proj1/GridViewAdapter.java
@@ -1,5 +1,57 @@
 package com.example.camp_proj1;
 
-public class GridViewAdapter {
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+public class GridViewAdapter extends BaseAdapter {
+    Context context = null;
+    int[] imageIDs = null;
+
+    public GridViewAdapter(Context context, int[] imageIDs){
+        this.context = context;
+        this.imageIDs = imageIDs;
+    }
+    @Override
+    public int getCount() {
+        return (null!=imageIDs)? imageIDs.length : 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return (null!=imageIDs)? imageIDs[position] : 0;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView = null;
+
+        if(null != convertView){
+            imageView = (ImageView) convertView;
+        }
+        else{
+            Bitmap bmp = BitmapFactory.decodeResource(context.getResources(), imageIDs[position]);
+            bmp = Bitmap.createScaledBitmap(bmp, 320, 240, false);
+
+            imageView = new ImageView(context);
+            imageView.setImageBitmap(bmp);
+
+            ImageClickListener imageViewClickListener = new ImageClickListener(context, imageIDs[position]);
+            imageView.setOnClickListener(imageViewClickListener);
+        }
+        return imageView;
+    }
+
 
 }
---
+++ app/src/main/java/com/example/camp_proj1/GridViewClickActivity.java
@@ -0,0 +1,38 @@
+package com.example.camp_proj1;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager.widget.ViewPager;
+
+public class GridViewClickActivity extends FragmentActivity {
+
+    private static int pagesNum = 0;
+    private ViewPager mPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gridviewclickevent);
+        ImageView imageView = (ImageView)findViewById(R.id.imageView);
+        setImage(imageView);
+
+    }
+
+    private void setImage(ImageView imageView) {
+
+        Intent receivedIntent = getIntent();
+        int imageID = (Integer)receivedIntent.getExtras().get("image ID");
+        imageView.setImageResource(imageID);
+    }
+
+
+
+
+}
